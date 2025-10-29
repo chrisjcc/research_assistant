cbc21194c967796ae6506ce443a9ac70e5f19a14
Christian Contreras Campana <chrisjcc.physics@gmail.com>
2025-10-29 22:04:31 +0100

    Fix ordering

--- src/research_assistant/type_utils/__init__.py
+++ src/research_assistant/type_utils/__init__.py
@@ -15,20 +15,44 @@ Example:
 """
 
 # Protocols
-from .protocols import (AnalystList, CacheProvider,  # Type aliases
-                        Checkpointer, Comparable, ConfigDict, ConfigProvider,
-                        ErrorHandler, LLMProvider, MessageList,
-                        MetricsCollector, MetricsDict, NodeExecutor,
-                        OutputFormatter, PromptFormatter, SearchProvider,
-                        SearchResults, Serializable, StateDict, StateUpdate,
-                        StateValidator, Validatable, ensure_protocol,
-                        implements_protocol)
+from .protocols import CacheProvider  # Type aliases
+from .protocols import (
+    AnalystList,
+    Checkpointer,
+    Comparable,
+    ConfigDict,
+    ConfigProvider,
+    ErrorHandler,
+    LLMProvider,
+    MessageList,
+    MetricsCollector,
+    MetricsDict,
+    NodeExecutor,
+    OutputFormatter,
+    PromptFormatter,
+    SearchProvider,
+    SearchResults,
+    Serializable,
+    StateDict,
+    StateUpdate,
+    StateValidator,
+    Validatable,
+    ensure_protocol,
+    implements_protocol,
+)
+
 # Validation
-from .validation import (TypeValidator, validate_dict_keys,
-                         validate_function_args, validate_list_length,
-                         validate_non_empty_string, validate_positive_number,
-                         validate_pydantic_model, validate_range,
-                         validate_type)
+from .validation import (
+    TypeValidator,
+    validate_dict_keys,
+    validate_function_args,
+    validate_list_length,
+    validate_non_empty_string,
+    validate_positive_number,
+    validate_pydantic_model,
+    validate_range,
+    validate_type,
+)
 
 __all__ = [
     # Protocols
